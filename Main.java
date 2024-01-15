dd24efbe1d138bc8397f6d9c020875421fb234b8
Antonino <pagana899@gmail.com>
2024-01-15 23:54:39 +0100

    Esercizio Completo 2

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ import java.time.OffsetDateTime;
 public class Main {
     public static void main(String[] args) {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        OffsetDateTime nuovaData = data.plusYears(1).minusMonths(1).plusDays(7);
-        System.out.println(nuovaData);
+    }
+    public OffsetDateTime formattaData(OffsetDateTime data){
+        OffsetDateTime formatDate = data.plusYears(1).minusMonths(1).plusDays(7);
+        return formatDate;
     }
 }
--- MainTest.java
+++ MainTest.java
@@ -5,12 +5,13 @@ import java.time.OffsetDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
+    Main testing = new Main();
     @Test
     void controlloData() {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        String actual = data.plusYears(1).minusMonths(1).plusDays(7).toString();
-        String expected = "2024-02-08T13:00Z";
-        assertEquals(expected, actual);
+        OffsetDateTime result = testing.formattaData(data);
+        OffsetDateTime expected = OffsetDateTime.parse("2024-02-08T13:00Z");
+        assertEquals(expected, result);
     }
 }
 
